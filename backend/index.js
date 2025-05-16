02ca1847fb873756c429727a585d12049641bb3e
MartinHaydenSK <martinhayden3030@gmail.com>
2025-05-16 17:09:28 +0200

    zvysok kodu odkomentovany

--- backend/index.js
+++ backend/index.js
@@ -31,107 +31,107 @@ app.get("/", (req, res) => {
   res.send("everything is working");
 });
 
-// app.post("/registration", async (req, res) => {
-//   console.log("called");
-//   const { name, surname, email, password } = req.body;
-//   const findUser = await UserTable.findOne({ email });
-//   if (findUser) {
-//     res.status(400).json("Použivateľ už existuje");
-//   } else {
-//     const createUser = await UserTable.create({
-//       name,
-//       surname,
-//       email,
-//       password,
-//     });
-//     if (createUser) {
-//       res.status(200).json("Boli ste úspešne zaregistorvaný");
-//     }
-//   }
-// });
+app.post("/registration", async (req, res) => {
+  console.log("called");
+  const { name, surname, email, password } = req.body;
+  const findUser = await UserTable.findOne({ email });
+  if (findUser) {
+    res.status(400).json("Použivateľ už existuje");
+  } else {
+    const createUser = await UserTable.create({
+      name,
+      surname,
+      email,
+      password,
+    });
+    if (createUser) {
+      res.status(200).json("Boli ste úspešne zaregistorvaný");
+    }
+  }
+});
 
-// app.post("/login", async (req, res) => {
-//   console.log("called");
-//   const { email, password } = req.body;
-//   console.log(req.body);
-//   const findUser = await UserTable.findOne({ email });
-//   if (findUser) {
-//     console.log(findUser, "hello");
-//     const comparison = await bcrypt.compare(password, findUser.password);
+app.post("/login", async (req, res) => {
+  console.log("called");
+  const { email, password } = req.body;
+  console.log(req.body);
+  const findUser = await UserTable.findOne({ email });
+  if (findUser) {
+    console.log(findUser, "hello");
+    const comparison = await bcrypt.compare(password, findUser.password);
 
-//     if (comparison) {
-//       const name = findUser.name;
-//       const surname = findUser.surname;
-//       const email = findUser.email;
-//       const payload = { name, surname, email };
-//       const token = createToken(payload);
-//       res.cookie("user", token, { maxAge: 24 * 60 * 60 * 1000 });
-//       res.status(200).json("Úspešene ste sa prihlásili");
-//     } else {
-//       res.status(200).json("Zlý email alebo heslo");
-//     }
-//   } else {
-//     console.log("called two");
-//     res.status(200).json("Zlý email alebo heslo");
-//   }
-// });
+    if (comparison) {
+      const name = findUser.name;
+      const surname = findUser.surname;
+      const email = findUser.email;
+      const payload = { name, surname, email };
+      const token = createToken(payload);
+      res.cookie("user", token, { maxAge: 24 * 60 * 60 * 1000 });
+      res.status(200).json("Úspešene ste sa prihlásili");
+    } else {
+      res.status(200).json("Zlý email alebo heslo");
+    }
+  } else {
+    console.log("called two");
+    res.status(200).json("Zlý email alebo heslo");
+  }
+});
 
-// app.post("/addPayer", async (req, res) => {
-//   try {
-//     const { email } = req.body;
-//     const findUser = await UserTable.findOneAndUpdate(
-//       { email },
-//       { isPayer: true },
-//       { new: true }
-//     );
-//     if (findUser) {
-//       console.log(findUser);
-//       res.status(200).json("Pridaný nový dlžník");
-//     }
-//   } catch (error) {
-//     console.log(error, "/addPayer");
-//   }
-// });
+app.post("/addPayer", async (req, res) => {
+  try {
+    const { email } = req.body;
+    const findUser = await UserTable.findOneAndUpdate(
+      { email },
+      { isPayer: true },
+      { new: true }
+    );
+    if (findUser) {
+      console.log(findUser);
+      res.status(200).json("Pridaný nový dlžník");
+    }
+  } catch (error) {
+    console.log(error, "/addPayer");
+  }
+});
 
-// app.post("/addToDept", async (req, res) => {
-//   const { id, NewAmount } = req.body;
-//   console.log("fired", req.body);
-//   try {
-//     const findUser = await UserTable.findOneAndUpdate(
-//       {
-//         _id: id,
-//       },
-//       { dept: NewAmount },
-//       { new: true }
-//     );
+app.post("/addToDept", async (req, res) => {
+  const { id, NewAmount } = req.body;
+  console.log("fired", req.body);
+  try {
+    const findUser = await UserTable.findOneAndUpdate(
+      {
+        _id: id,
+      },
+      { dept: NewAmount },
+      { new: true }
+    );
 
-//     if (findUser) {
-//       res.status(200).json("Úspešne bola pridaná hodnota");
-//     }
-//   } catch (error) {
-//     console.log(error, "/addToDept");
-//   }
-// });
-// app.get("/getuser", async (req, res) => {
-//   try {
-//     const token = req.cookies.user;
-//     const decodedToken = jwt.verify(token, "mhjekral");
+    if (findUser) {
+      res.status(200).json("Úspešne bola pridaná hodnota");
+    }
+  } catch (error) {
+    console.log(error, "/addToDept");
+  }
+});
+app.get("/getuser", async (req, res) => {
+  try {
+    const token = req.cookies.user;
+    const decodedToken = jwt.verify(token, "mhjekral");
 
-//     if (decodedToken) {
-//       res.status(200).json(decodedToken.data);
-//     }
-//   } catch (error) {
-//     console.log(error, "/getuser");
-//   }
-// });
+    if (decodedToken) {
+      res.status(200).json(decodedToken.data);
+    }
+  } catch (error) {
+    console.log(error, "/getuser");
+  }
+});
 
-// app.get("/getusers", async (req, res) => {
-//   try {
-//     const findUsers = await UserTable.find();
-//     if (findUsers) {
-//       res.status(200).json(findUsers);
-//     }
-//   } catch (error) {
-//     console.log(error, "/getusers");
-//   }
-// });
+app.get("/getusers", async (req, res) => {
+  try {
+    const findUsers = await UserTable.find();
+    if (findUsers) {
+      res.status(200).json(findUsers);
+    }
+  } catch (error) {
+    console.log(error, "/getusers");
+  }
+});
